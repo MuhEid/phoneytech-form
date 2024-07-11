f673b538d51bd40e3c4f390c75aa4ebe477dcd9c
MuhEid96 <moh.eid.1996@gmail.com>
2024-07-11 17:39:07 +0300

    feat: initial backend logic to create an api for form submision

---
+++ src/server/dbConnection.ts
@@ -0,0 +1,17 @@
+import { connect } from "mongoose"
+
+const options = {
+    dbName: "handytechs",
+};
+
+const db_connection = () => {
+    connect(`${process.env.CONNECTION_STRING}`, options)
+        .then((connect) => {
+            console.log(`Success Connect to Database: ${connect.connection.host}`);
+        })
+        .catch((error) => {
+            console.error(`Error connecting to MongoDB: ${error}`);
+            setTimeout(db_connection, 5000);
+        });
+};
+export default db_connection;

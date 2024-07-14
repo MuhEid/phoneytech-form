ea2d500f4a6a302c6a3ed2968f6e03ac509d4ec1
MuhEid96 <moh.eid.1996@gmail.com>
2024-07-14 15:37:36 +0300

    chore: some style edits

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,17 @@ const config: Config = {
                 "gradient-conic":
                     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
             },
+            container: {
+                center: true,
+                padding: "3rem", // default padding
+                screens: {
+                    sm: "100%", // full width on small screens
+                    md: "100%", // full width on medium screens
+                    lg: "800px", // 800px width on large screens
+                    xl: "1200px", // 1200px width on extra-large screens
+                    "2xl": "1200px", // 1400px width on 2x extra-large screens
+                },
+            },
         },
     },
     plugins: [require("flowbite/plugin")],

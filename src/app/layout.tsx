5b559763d7cb19568702318d5c54208ca878af03
MuhEid96 <moh.eid.1996@gmail.com>
2025-10-31 16:45:08 +0200

    remove FormDataProvider from RootLayout in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { FormDataProvider } from "@/context/FormDataContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +17,7 @@ export default function RootLayout({
     return (
         <html lang="en" data-theme="light">
             <body className={inter.className}>
-                <FormDataProvider>{children}</FormDataProvider>
+                {children}
             </body>
         </html>
     );
